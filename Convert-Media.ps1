e2712d5c2cedb81403e71c7b530677b373034635
Paul Boyer <prboyer15@gmail.com>
2021-10-23 19:19:24 -0500

    Overhaul of media conversion script
    - Moved Handbrake parameters to separate JSON config file
    - Changed script to not process the encoding as a PS Job
    - Condensed script to remove unnecessary logic
    - Removed -Quiet parameter
    - Updated comment-based help
    Changes to be committed:
    	modified:   .gitignore
    	modified:   Convert-Media.ps1
    	new file:   Settings.json

--- Convert-Media.ps1
+++ Convert-Media.ps1
@@ -1,3 +1,4 @@
+    function Convert-Media{
     <#
     .SYNOPSIS
         Script to automate the process of transcoding video files.
@@ -8,9 +9,7 @@
     .Parameter FilePath
         This should be the fully qualified path to the imput file.
     .Parameter Destination
-        This is the path the the output directory. It does not need to be fully qualified. 
-    .Parameter Quiet
-        Specifying the quiet parameter will prevent the script from showing the progress of the conversion in a console window. 
+        This is the path the the output directory. It does not need to be fully qualified.
     .EXAMPLE
         PS C:\> Powershell.exe -NoProfile -NoLogo -ExecutionPolicy Bypass -File .\Convert-Media.ps1 -FilePath ".\movie.wmv" -Destination ".\Folder\"
         Typical use on the command line using relative paths
@@ -27,58 +26,39 @@
     #>
     param (
         [Parameter(Position=0,Mandatory=$true)]
+        [ValidateScript({
+            # Check if the file is already an MP4.
+            if ([System.IO.Path]::GetExtension($FilePath) -notlike "mp4") {return $true} else {return $false}
+        })]
         [String]
         $FilePath,
         [Parameter(Position=1,Mandatory=$true)]
+        [ValidateScript({
+            # Check if the detination path exists and is a directory
+            if (Test-Path -Path $(Resolve-Path -Path $_) -PathType Container){return $true} else {return $false}
+        })]
         [String]
-        $Destination,
-        [Parameter()]
-        [switch]
-        $Quiet
-
+        $Destination
     )
-    # If the destination parameter isn't provided, create the new file in the same directory as the source
-    if ($Destination -eq "") {
-        $Destination = $(Split-Path -Path $FilePath -Parent);
+    #Requires -Version 5.1
+
+    # Import the settings from the configuration file
+    [Object]$Settings = ConvertFrom-Json -InputObject $(Get-Content $PSScriptRoot\Settings.json -Raw)
+    
+    # Generate the name of the output file
+    [String]$OutFile = "$($Destination)\$([System.IO.Path]::GetFileNameWithoutExtension($FilePath)).mp4"
+
+    Write-Information ("`nCalling Handbrake for Converstion - {0}" -f $(Split-Path $FilePath -Leaf)) -InformationAction Continue
+
+    try{
+        # Begin the video processing by calling Handbrake
+        Start-Process -FilePath $Settings.Handbrake.CLI -NoNewWindow -Wait -ArgumentList "--input `"$($FilePath)`" --output `"$($OutFile)`" --format $($Settings.Handbrake.Conversion.Format) --encoder $($Settings.Handbrake.Conversion.Encoder) --preset `"$($Settings.Handbrake.Conversion.Preset)`" --main-feature --verbose"
+
+        # Notify that the file has completed without error
+        Write-Information "Processing Complete" -InformationAction Continue
+
+    }catch{
+        Write-Error $("Error processing file with Handbrake `n{0}" -f $ERROR)
+
     }
-
-    # Make sure to expand the path
-    $Destination = Resolve-Path -Path $Destination
-
-    <# Constants #>
-        # Path to Handbrake CLI executable
-        [String]$HANDBRAKE_CLI = "$env:ProgramFiles\HandbrakeCLI\HandbrakeCLI.exe"
-
-    # Check if the file is already an MP4. If yes, then don't run through Handbrake
-    if ([System.IO.Path]::GetExtension($FilePath) -notlike "mp4") {
-        Write-Information ("`nCalling Handbrake for Converstion - {0}" -f $FilePath) -InformationAction Continue
-
-        $ConversionJob = Start-Job -Name $("Handbrake Conversion Job - {0}" -f $(Split-Path -Path $FilePath -Leaf)) -ArgumentList $HANDBRAKE_CLI, $FilePath, $Destination -ScriptBlock {
-            try{
-                [String]$ArgumentList = ("-v -i `""+$args[1]+"`" -o `""+$args[2]+"\"+$args[1].ToString().Substring($args[1].ToString().LastIndexOf('\'),$args[2].ToString().Length-4)+".mp4`" -f av_mp4 -e vce_h265 -Z `"H.265 MKV 2160p60`"")
-
-                # Write out the command line call that is processing the file     
-                Write-Information ($args[0]+$ArgumentList)            
-                
-                # Actually start processing the file
-                Write-Host ("Start Processing {0}" -f $File) -ForegroundColor Yellow
-                
-                # Supress the console window if -Quiet is passed
-                if($Quiet){
-                    Start-Process -FilePath $args[0] -Wait -ArgumentList $ArgumentList -NoNewWindow
-                }else{
-                    Start-Process -FilePath $args[0] -Wait -ArgumentList $ArgumentList
-                }
-                
-                # Notify that the file has completed without error
-                Write-Information "Processing Complete" -InformationAction Continue
-            }catch{
-                Write-Error $("Error processing file with Handbrake `n{0}" -f $ERROR)
-            }
-        }
-    }else{
-        Write-Host "File is already an MP4" -ForegroundColor Yellow
-    }
-
-    # Wait-Job -Job $ConversionJob
-    # Receive-Job -Job $ConversionJob
+}
